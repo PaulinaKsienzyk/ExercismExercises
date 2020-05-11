5b7a9c1fde28efeeec59548915c16757640f8910
Paulina Ksienzyk <pilnicka8@gmail.com>
2020-05-11 20:50:14 +0200

    add Scrabble.java

---
+++ scrabble-score/src/main/java/Scrabble.java
@@ -0,0 +1,56 @@
+import java.util.Map;
+
+import static java.util.Map.entry;
+
+class Scrabble {
+    private int score = 0;
+
+    Scrabble(String word) {
+        char[] chars = word.toUpperCase().toCharArray();
+
+        Map<Character, Integer> letters = Map.ofEntries(
+                entry('A', 1),
+                entry('O', 1),
+                entry('E', 1),
+                entry('I', 1),
+                entry('U', 1),
+                entry('L', 1),
+                entry('N', 1),
+                entry('R', 1),
+                entry('S', 1),
+                entry('T', 1),
+
+                entry('D', 2),
+                entry('G', 2),
+
+                entry('B', 3),
+                entry('C', 3),
+                entry('M', 3),
+                entry('P', 3),
+
+                entry('F', 4),
+                entry('H', 4),
+                entry('V', 4),
+                entry('W', 4),
+                entry('Y', 4),
+
+                entry('K', 5),
+
+                entry('J', 8),
+                entry('X', 8),
+
+                entry('Q', 10),
+                entry('Z', 10)
+        );
+
+        for (char aChar : chars) {
+            this.score += letters.get(aChar);
+        }
+    }
+
+    int getScore() {
+        return this.score;
+    }
+
+
+}
